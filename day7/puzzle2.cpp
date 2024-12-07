620bea0129cc05e62d6ebb387a9a1bf1c264bc60
SungHunYoon <gnsals6004@gmail.com>
2024-12-07 15:50:15 +0900

    add: day7

---
+++ day7/puzzle2.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <sstream>
+#include <vector>
+
+using namespace std;
+
+bool dfs(vector<long> &v, int idx, long val, long val2) {
+	if (idx >= v.size()) {
+		if (val == val2)
+			return true;
+		return false;
+	}
+	if (dfs(v, idx + 1, val + v[idx], val2))
+		return true;
+	if (dfs(v, idx + 1, val * v[idx], val2))
+		return true;
+	return dfs(v, idx + 1, stol(to_string(val) + to_string(v[idx])), val2);
+}
+
+int main(void) {
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	string S;
+	long answer = 0;
+	while (getline(cin, S)) {
+		stringstream ss;
+		long num = stol(S);
+		ss << S.substr(S.find(": ", 0) + 2);
+		vector<long> V;
+		while (!ss.eof()) {
+			long n;
+			ss >> n;
+			V.push_back(n);
+		}
+		if (dfs(V, 1, V[0], num))
+			answer += num;
+	}
+	cout << answer << '\n';
+	return 0;
+}
